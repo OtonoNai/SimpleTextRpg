a0af530023b09b906d34f79587139029e9fdf4d4
OtonoNai. <otononai2x5@gmail.com>
2026-07-16 20:50:13 +0900

    refactor: replace Player's map-based status with typed fields

--- Src/Player.cpp
+++ Src/Player.cpp
@@ -1,26 +1,138 @@
 #include "Player.h"
 
+FPlayer::FPlayer()
+    : Name(""), Hp(0), Mp(0), Atk(0), Def(0)
+{
+}
+
+FPlayer::FPlayer(std::string InName, int InHp, int InMp, int InAtk, int InDef)
+	: Name(InName), Hp(InHp), Mp(InMp), Atk(InAtk), Def(InDef)
+{
+}
+
 std::map<std::string, std::string> FPlayer::GetAllStatus()
 {
-    return Status;
+	std::map<std::string, std::string> Result;
+	Result["name"] = Name;
+	Result["hp"] = std::to_string(Hp);
+	Result["mp"] = std::to_string(Mp);
+	Result["atk"] = std::to_string(Atk);
+	Result["def"] = std::to_string(Def);
+	return Result;
 }
 
 std::string FPlayer::GetStatusFromKey(std::string Key)
 {
-    return Status[Key];
+    if (Key == "name")
+    {
+        return Name;
+    }
+    else if (Key == "hp")
+    {
+        return std::to_string(Hp);
+    }
+    else if (Key == "mp")
+    {
+        return std::to_string(Mp);
+    }
+    else if (Key == "atk")
+    {
+        return std::to_string(Atk);
+    }
+    else if (Key == "def")
+    {
+        return std::to_string(Def);
+    }
+
+    return "";
 }
 
 void FPlayer::SetStatus(std::string Key, std::string Value)
 {
-    Status[Key] = Value;
+    if (Key == "name")
+    {
+        Name = Value;
+    }
+    else if (Key == "hp")
+    {
+        Hp = stoi(Value);
+    }
+    else if (Key == "mp")
+    {
+        Mp = stoi(Value);
+    }
+    else if (Key == "atk")
+    {
+        Atk = stoi(Value);
+    }
+    else if (Key == "def")
+    {
+        Def = stoi(Value);
+    }
+}
+
+void FPlayer::ModifyStat(std::string StatName, std::string Operation, int Value)
+{
+    int* Target = nullptr;
+
+    if (StatName == "hp")
+    {
+        Target = &Hp;
+    }
+    else if (StatName == "mp")
+    {
+        Target = &Mp;
+    }
+    else if (StatName == "atk")
+    {
+        Target = &Atk;
+    }
+    else if (StatName == "def")
+    {
+        Target = &Def;
+    }
+
+    if (Target == nullptr)
+    {
+        return;
+    }
+
+    if (Operation == "add")
+    {
+        *Target += Value;
+    }
+    else if (Operation == "multiply")
+    {
+        *Target *= Value;
+    }
 }
 
 bool FPlayer::GetIsValidStatus()
 {
-    return bIsValidStatus;
+	return bIsValidStatus;
 }
 
 void FPlayer::SetIsValidStatus(bool bInValue)
 {
-    bIsValidStatus = bInValue;
+	bIsValidStatus = bInValue;
+}
+
+int FPlayer::GetHp() const
+{
+    return Hp;
+}
+
+int FPlayer::GetMp() const
+{
+    return Mp;
+}
+
+int FPlayer::GetAtk() const
+{
+    return Atk;
+}
+
+int FPlayer::GetDef() const
+{
+    return Def;
 }
--- Src/Player.h
+++ Src/Player.h
@@ -6,13 +6,27 @@
 class FPlayer
 {
 public:
+    FPlayer();
+    FPlayer(std::string InName, int InHp, int InMp, int InAtk, int InDef);
+
     std::map<std::string, std::string> GetAllStatus();
     std::string GetStatusFromKey(std::string Key);
     void SetStatus(std::string Key, std::string Value);
+    void ModifyStat(std::string StatName, std::string Operation, int Value);
+
     bool GetIsValidStatus();
     void SetIsValidStatus(bool bInValue);
 
+    int GetHp() const;
+    int GetMp() const;
+    int GetAtk() const;
+    int GetDef() const;
+
 private:
-    std::map<std::string, std::string> Status;
+    std::string Name;
+    int Hp;
+    int Mp;
+    int Atk;
+    int Def;
     bool bIsValidStatus = false;
 };
